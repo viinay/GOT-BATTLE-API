02e8ccbf8904b83fbcf80a9f5a58465fd36ac4f3
viinay <viinay@users.noreply.github.com>
2018-11-27 18:59:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
   res.send("on home page");
 })
 app.get('/list', (req, response) => {
-  let query = 'Select location FROM battles7 WHERE location is NOT NULL AND location <> ""';
+  let query = 'Select location FROM battles WHERE location is NOT NULL AND location <> ""';
   con.connect(function (err) {
     if (err) {
       console.log("Error while connecting database :- " + err);
@@ -63,7 +63,7 @@ app.get('/list', (req, response) => {
 
 app.get('/count', (req, response) => {
 
-  let query = 'select count(battle_number) as count from battles7;'
+  let query = 'select count(battle_number) as count from battles;'
   con.connect(function (err) {
     if (err) {
       console.log("Error while connecting database :- " + err);
@@ -87,7 +87,7 @@ app.get('/stats', (req, response) => {
   
       // query to the database;;
           let resObj = [];
-          let query1 = "SELECT attacker_outcome, count(attacker_outcome) as count FROM battles7 GROUP BY attacker_outcome"
+          let query1 = "SELECT attacker_outcome, count(attacker_outcome) as count FROM battles GROUP BY attacker_outcome"
           let query = 'select avg(defender_size) as average, min(defender_size) as minimum, max(defender_size) as maximum from battles7;'
           
           con.query(query, function(error, result){
@@ -108,7 +108,7 @@ app.get('/stats', (req, response) => {
 
             }
           })
-            let query2 = "select attacker_king, count(attacker_king) as count FROM battles7 GROUP BY attacker_king ORDER BY count DESC LIMIT 1;"
+            let query2 = "select attacker_king, count(attacker_king) as count FROM battles GROUP BY attacker_king ORDER BY count DESC LIMIT 1;"
               con.query(query2,function(err, result2){
                 if(err){
                   return err
@@ -118,7 +118,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query3 = "select defender_king, count(defender_king) as count FROM battles7 GROUP BY defender_king ORDER BY count DESC LIMIT 1;"
+              let query3 = "select defender_king, count(defender_king) as count FROM battles GROUP BY defender_king ORDER BY count DESC LIMIT 1;"
               con.query(query3,function(err, result3){
                 if(err){
                   return err
@@ -128,7 +128,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query4 = "select region, count(region) as count FROM battles7 GROUP BY region ORDER BY count DESC LIMIT 1;"
+              let query4 = "select region, count(region) as count FROM battles GROUP BY region ORDER BY count DESC LIMIT 1;"
               con.query(query4,function(err, result4){
                 if(err){
                   return err
@@ -138,7 +138,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query5 = "select name, count(name) as count FROM battles7 GROUP BY name ORDER BY count DESC LIMIT 1;"
+              let query5 = "select name, count(name) as count FROM battles GROUP BY name ORDER BY count DESC LIMIT 1;"
               con.query(query5,function(err, result5){
                 if(err){
                   return err
@@ -148,7 +148,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query6 = "select distinct battle_type type FROM battles7 ;"
+              let query6 = "select distinct battle_type type FROM battles ;"
               con.query(query6,function(err, result6){
                 if(err){
                   return err
@@ -179,7 +179,7 @@ app.get('/battle-results', (req, res) => {
 }
   
   searchquery = searchquery.substring(0, searchquery.length - 3)
-  let query = "select name from battles7 where" + searchquery;
+  let query = "select name from battles where" + searchquery;
   con.query(query, function(err, result){
     if(err) return console.log(err);
 
