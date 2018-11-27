e2b4419951ccd9fff4feb296888df64da187601f
viinay <viinay@users.noreply.github.com>
2018-11-27 18:57:41 +0530

    Update csvDataLoad.js

--- csvDataLoad.js
+++ csvDataLoad.js
@@ -27,11 +27,11 @@ let csvStream = csv
             } else {
                 console.log("came in ");
                 
-                createQuery = 'create table battles7 (name varchar(500),year varchar(500),battle_number varchar (6),attacker_king varchar(500),defender_king varchar(500),attacker_1 varchar(500),attacker_2 varchar(500),attacker_3 varchar(500),attacker_4 varchar(500),defender_1 varchar(500),defender_2 varchar(500),defender_3 varchar(500),defender_4 varchar(500),attacker_outcome varchar(500),battle_type varchar(500),major_death varchar(500),major_capture varchar(500),attacker_size varchar(500),defender_size varchar(100),attacker_commander varchar(500),defender_commander varchar(500),summer varchar(500),location varchar(500),region varchar(500),note varchar(500));';
+                createQuery = 'create table battles (name varchar(500),year varchar(500),battle_number varchar (6),attacker_king varchar(500),defender_king varchar(500),attacker_1 varchar(500),attacker_2 varchar(500),attacker_3 varchar(500),attacker_4 varchar(500),defender_1 varchar(500),defender_2 varchar(500),defender_3 varchar(500),defender_4 varchar(500),attacker_outcome varchar(500),battle_type varchar(500),major_death varchar(500),major_capture varchar(500),attacker_size varchar(500),defender_size varchar(100),attacker_commander varchar(500),defender_commander varchar(500),summer varchar(500),location varchar(500),region varchar(500),note varchar(500));';
                 connection.query(createQuery, (err, res) => {
                     if (err) return console.log(err);
                     console.log("was came in ");
-                    let query = 'INSERT INTO battles7 (name,year,battle_number,attacker_king,defender_king,attacker_1,attacker_2,attacker_3,attacker_4,defender_1,defender_2,defender_3,defender_4,attacker_outcome,battle_type,major_death,major_capture,attacker_size,defender_size,attacker_commander,defender_commander,summer,location,region,note) VALUES ?';
+                    let query = 'INSERT INTO battles (name,year,battle_number,attacker_king,defender_king,attacker_1,attacker_2,attacker_3,attacker_4,defender_1,defender_2,defender_3,defender_4,attacker_outcome,battle_type,major_death,major_capture,attacker_size,defender_size,attacker_commander,defender_commander,summer,location,region,note) VALUES ?';
                     connection.query(query, [myData], (error, response) => {
                         if(error) return console.log(error)
                         
