f2cf57ea21e6010c5f5cc71683c0a46f1f131de7
viinay <viinay@users.noreply.github.com>
2018-11-28 12:04:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,78 +91,110 @@ app.get('/stats', (req, response) => {
           let resObj = [];
           let query1 = "SELECT attacker_outcome, count(attacker_outcome) as count FROM battles101 GROUP BY attacker_outcome"
           let query = 'select avg(defender_size) as average, min(defender_size) as minimum, max(defender_size) as maximum from battles101;'
+          var promise1 = new Promise ((resolve, reject) =>{
+            con.query(query, function(error, result){
+              if (error) {
+                return reject(error);
+
+              }
+              else {
+              //  resObj= resObj.concat(result)
+               resolve(result)
+              }
+            })
+          })
           
-          con.query(query, function(error, result){
-            if (error) {
-              return error;
-            }
-            else {
-             resObj= resObj.concat(result)
-            }
+          var promise2 = new Promise ((resolve, reject)=>{
+            con.query(query1, function(error, result1){
+              if (error) {
+                
+                return reject(error);
+              }
+              else {
+                // resObj= resObj.concat(result1)
+                resolve(result1)
+              }
+            })
           })
+          
+          var promise3 = new Promise ((resolve, reject)=>{
 
-          con.query(query1, function(error, result1){
-            if (error) {
-              return error;
-            }
-            else {
-              resObj= resObj.concat(result1)
-
-            }
-          })
             let query2 = "select attacker_king, count(attacker_king) as count FROM battles101 GROUP BY attacker_king ORDER BY count DESC LIMIT 1;"
               con.query(query2,function(err, result2){
                 if(err){
-                  return err
+                  return reject(err)
                 }
                 else{
-                  resObj= resObj.concat(result2)
+                  // resObj= resObj.concat(result2)
+                  resolve(result2)
                 }
               })
+            })
+
+          var promise4 = new Promise ((resolve, reject)=>{
 
               let query3 = "select defender_king, count(defender_king) as count FROM battles101 GROUP BY defender_king ORDER BY count DESC LIMIT 1;"
               con.query(query3,function(err, result3){
                 if(err){
-                  return err
+                  return reject(err)
                 }
                 else{
-                  resObj= resObj.concat(result3)
+                  // resObj= resObj.concat(result3)
+                  resolve(result3)
                 }
               })
+            })
+
+          var promise5 = new Promise ((resolve, reject)=>{
 
               let query4 = "select region, count(region) as count FROM battles101 GROUP BY region ORDER BY count DESC LIMIT 1;"
               con.query(query4,function(err, result4){
                 if(err){
-                  return err
+                  return reject('no region')
                 }
                 else{
-                  resObj= resObj.concat(result4)
+                  // resObj= resObj.concat(result4)
+                  resolve(result4)
                 }
               })
+            })
 
-              let query5 = "select name, count(name) as count FROM battles101 GROUP BY name ORDER BY count DESC LIMIT 1;"
+          var promise6 = new Promise ((resolve, reject)=>{
+
+              let query5 = "select name, count(name) as count FROM battles101 GROUP BY name ORDER BY name DESC LIMIT 1;"
               con.query(query5,function(err, result5){
                 if(err){
-                  return err
+                  return reject(err)
                 }
                 else{
-                  resObj= resObj.concat(result5)
+                  // resObj= resObj.concat(result5)
+                  resolve(result5)
                 }
               })
+            })
+
+          var promise7 = new Promise ((resolve, reject)=>{
 
               let query6 = "select distinct battle_type type FROM battles101 ;"
               con.query(query6,function(err, result6){
                 if(err){
-                  return err
+                  return reject(err)
                 }
                 else{
-                  resObj= resObj.concat(result6)
+                  console.log(result6)
+                  // resObj= resObj.concat(result6)
+                  resolve(result6)
                 }
               })
+            })
 
-              setTimeout(() => {
-                response.send(resObj)
-              }, 1500);
+              Promise.all([promise1,promise2,promise3,promise4,promise5,promise6,promise7])
+                .then(function(results){
+                  response.json(results)
+                })
+                .catch(function(err){
+                  response.json(err.message)
+                })
       })
 
 
