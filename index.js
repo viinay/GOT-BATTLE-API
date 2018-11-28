a145256d6331b0c209b165c45f8a603cf9e4735b
viinay <viinay@users.noreply.github.com>
2018-11-28 11:06:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,11 +36,12 @@ var executeQuery = function(res, query){
   });
 }
 
+
 app.get('/', (req, res) => {
   res.send("on home page");
 })
 app.get('/list', (req, response) => {
-  let query = 'Select location FROM battles WHERE location is NOT NULL AND location <> ""';
+  let query = 'Select location FROM battles101 WHERE location is NOT NULL AND location <> ""';
   con.connect(function (err) {
     if (err) {
       console.log("Error while connecting database :- " + err);
@@ -64,7 +65,7 @@ app.get('/list', (req, response) => {
 
 app.get('/count', (req, response) => {
 
-  let query = 'select count(battle_number) as count from battles;'
+  let query = 'select count(battle_number) as count from battles101;'
   con.connect(function (err) {
     if (err) {
       console.log("Error while connecting database :- " + err);
@@ -88,8 +89,8 @@ app.get('/stats', (req, response) => {
   
       // query to the database;;
           let resObj = [];
-          let query1 = "SELECT attacker_outcome, count(attacker_outcome) as count FROM battles GROUP BY attacker_outcome"
-          let query = 'select avg(defender_size) as average, min(defender_size) as minimum, max(defender_size) as maximum from battles7;'
+          let query1 = "SELECT attacker_outcome, count(attacker_outcome) as count FROM battles101 GROUP BY attacker_outcome"
+          let query = 'select avg(defender_size) as average, min(defender_size) as minimum, max(defender_size) as maximum from battles101;'
           
           con.query(query, function(error, result){
             if (error) {
@@ -109,7 +110,7 @@ app.get('/stats', (req, response) => {
 
             }
           })
-            let query2 = "select attacker_king, count(attacker_king) as count FROM battles GROUP BY attacker_king ORDER BY count DESC LIMIT 1;"
+            let query2 = "select attacker_king, count(attacker_king) as count FROM battles101 GROUP BY attacker_king ORDER BY count DESC LIMIT 1;"
               con.query(query2,function(err, result2){
                 if(err){
                   return err
@@ -119,7 +120,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query3 = "select defender_king, count(defender_king) as count FROM battles GROUP BY defender_king ORDER BY count DESC LIMIT 1;"
+              let query3 = "select defender_king, count(defender_king) as count FROM battles101 GROUP BY defender_king ORDER BY count DESC LIMIT 1;"
               con.query(query3,function(err, result3){
                 if(err){
                   return err
@@ -129,7 +130,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query4 = "select region, count(region) as count FROM battles GROUP BY region ORDER BY count DESC LIMIT 1;"
+              let query4 = "select region, count(region) as count FROM battles101 GROUP BY region ORDER BY count DESC LIMIT 1;"
               con.query(query4,function(err, result4){
                 if(err){
                   return err
@@ -139,7 +140,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query5 = "select name, count(name) as count FROM battles GROUP BY name ORDER BY count DESC LIMIT 1;"
+              let query5 = "select name, count(name) as count FROM battles101 GROUP BY name ORDER BY count DESC LIMIT 1;"
               con.query(query5,function(err, result5){
                 if(err){
                   return err
@@ -149,7 +150,7 @@ app.get('/stats', (req, response) => {
                 }
               })
 
-              let query6 = "select distinct battle_type type FROM battles ;"
+              let query6 = "select distinct battle_type type FROM battles101 ;"
               con.query(query6,function(err, result6){
                 if(err){
                   return err
@@ -180,7 +181,7 @@ app.get('/battle-results', (req, res) => {
 }
   
   searchquery = searchquery.substring(0, searchquery.length - 3)
-  let query = "select name from battles where" + searchquery;
+  let query = "select name from battles101 where" + searchquery;
   con.query(query, function(err, result){
     if(err) return console.log(err);
 
